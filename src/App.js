3f5269448ac79a4e93c845aec106615b51d7c5a1
msunaramadhanilmii <msunaramadhanilmi@gmail.com>
2023-02-16 17:35:38 +0700

    add package rn navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {SafeAreaView,StatusBar, Text} from 'react-native';
 import { SplashScreen } from './pages';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
   return (
-    <SplashScreen />
+    <NavigationContainer>
+      <SplashScreen />
+    </NavigationContainer>
   );
 };
 
