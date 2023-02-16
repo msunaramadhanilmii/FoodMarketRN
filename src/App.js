9220e4683208bbbe8c70c77aae70d21510e87c81
msunaramadhanilmii <msunaramadhanilmi@gmail.com>
2023-02-16 19:04:29 +0700

    slicing page sign in

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {SafeAreaView,StatusBar, Text} from 'react-native';
-import { SplashScreen } from './pages';
+import { SignIn, SplashScreen } from './pages';
 import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <SplashScreen />
+      {/* <SplashScreen /> */}
+      <SignIn />
     </NavigationContainer>
   );
 };
