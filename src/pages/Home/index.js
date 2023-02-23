8853c4cbeb1c70edeee88cd6c88981d88d3ce0c9
msunaramadhanilmii <msunaramadhanilmi@gmail.com>
2023-02-23 14:54:47 +0700

    welcoming user home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View, Image} from 'react-native';
+import React from 'react';
+import {ProfileDummy} from '../../assets';
 
 const Home = () => {
   return (
     <View>
+      <View style={styles.profileContainer}>
+        <View>
+          <Text style={styles.appName}>FoodMarket</Text>
+          <Text style={styles.desc}>Let's get some foods</Text>
+        </View>
+        <Image source={ProfileDummy} style={styles.profile} />
+      </View>
       <Text>Home Page Screen</Text>
     </View>
-  )
-}
+  );
+};
 
 export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  profileContainer: {flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 24, paddingTop: 32, paddingBottom: 24, backgroundColor: 'white'},
+  appName: {fontSize: 22, fontFamily: 'Poppins-Meidum', color: '#020202'},
+  desc: {fontSize: 14, fontFamily: 'Poppins-Light', color: '#8D92A3'},
+  profile: {width: 50, height: 50, borderRadius: 8},
+});
--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ import {
   Order,
   Profile,
 } from '../pages';
-import { BottomNavigator } from '../components';
+import {BottomNavigator} from '../components';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -19,12 +19,12 @@ const Tab = createBottomTabNavigator();
 const MainApp = () => {
   return (
     <Tab.Navigator tabBar={props => <BottomNavigator {...props} />}>
-      <Tab.Screen name="Home" component={Home}/>
+      <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Order" component={Order} />
       <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
-  )
-}
+  );
+};
 
 const Router = () => {
   return (
