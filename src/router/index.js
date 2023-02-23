28159fdcde47c7ac2fae0be87c24eca3c3b95a14
msunaramadhanilmii <msunaramadhanilmi@gmail.com>
2023-02-23 13:26:14 +0700

    tab navigation home,order & profile

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {
   SignIn,
   SignUp,
   SignUpAddress,
   SplashScreen,
   SuccessSignUp,
+  Home,
+  Order,
+  Profile,
 } from '../pages';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home}/>
+      <Tab.Screen name="Order" component={Order} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  )
+}
 
 const Router = () => {
   return (
@@ -36,6 +51,11 @@ const Router = () => {
       <Stack.Screen
         name="SuccessSignUp"
         component={SuccessSignUp}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="MainApp"
+        component={MainApp}
         options={{headerShown: false}}
       />
     </Stack.Navigator>
